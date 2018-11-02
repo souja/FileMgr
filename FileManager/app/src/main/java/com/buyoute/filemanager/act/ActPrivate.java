beff4c62e4fcdd9f85e6d9ae8713de56f31f7db0
yangdezhao <782579195@qq.com>
2018-11-02 13:42:11 +0800

    222

--- FileManager/app/src/main/java/com/buyoute/filemanager/act/ActPrivate.java
+++ FileManager/app/src/main/java/com/buyoute/filemanager/act/ActPrivate.java
@@ -1,12 +1,13 @@
 package com.buyoute.filemanager.act;
 
+import com.buyoute.filemanager.R;
 import com.buyoute.filemanager.base.ActBase;
 
 public class ActPrivate extends ActBase {
 
     @Override
     public void onCreate() {
-
+        setContentView(R.layout.act_private);
     }
 
     @Override
